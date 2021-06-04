47ad910bf67993cb6643b382701239c80b24836e
ryudongjae <rdj10149@gmail.com>
2021-06-04 18:20:07 +0900

    quickSort(left,right,middle)

--- src/algorithm/quickSort/MiddlePivotQuickSort.java
+++ src/algorithm/quickSort/MiddlePivotQuickSort.java
@@ -1,26 +1,20 @@
 package algorithm.quickSort;
 
 public class MiddlePivotQuickSort {
-    public static void sort(int[]a){
-        r_pivot_sort(a,0,a.length-1);
+
+    public static void sort(int[] a) {
+        m_pivot_sort(a, 0, a.length - 1);
     }
 
-    /**
-     *  오른쪽 피벗 선택 방식
-     * @param a		정렬할 배열
-     * @param lo	현재 부분배열의 왼쪽
-     * @param hi	현재 부분배열의 오른쪽
-     */
-    private static void r_pivot_sort(int[]a,int lo,int hi){
+    private static void m_pivot_sort(int[] a, int lo, int hi) {
+
         /*
          *  lo가 hi보다 크거나 같다면 정렬 할 원소가
          *  1개 이하이므로 정렬하지 않고 return한다.
          */
-
-        if(lo>= hi){
+        if(lo >= hi){
             return;
         }
-
         /*
          * 피벗을 기준으로 요소들이 왼쪽과 오른쪽으로 약하게 정렬 된 상태로
          * 만들어 준 뒤, 최종적으로 pivot의 위치를 얻는다.
@@ -32,9 +26,9 @@ public class MiddlePivotQuickSort {
          *
          * Partitioning:
          *
-         *         left part                right part       a[right]
+         *      left part      a[(right + left)/2]      right part
          * +---------------------------------------------------------+
-         * |    element < pivot    |    element >= pivot   |  pivot  |
+         * |    element < pivot    |  pivot  |    element >= pivot   |
          * +---------------------------------------------------------+
          *
          *
@@ -51,67 +45,69 @@ public class MiddlePivotQuickSort {
          *
          *  Recursion:
          *
-         * r_pivot_sort(a, lo, pivot - 1)     r_pivot_sort(a, pivot + 1, hi)
+         * m_pivot_sort(a, lo, pivot)         m_pivot_sort(a, pivot + 1, hi)
          *
          *         left part                           right part
          * +-----------------------+             +-----------------------+
-         * |   element <= pivot    |    pivot    |    element > pivot    |
+         * |   element <= pivot    |             |    element > pivot    |
          * +-----------------------+             +-----------------------+
-         * lo                pivot - 1        pivot + 1                 hi
+         * lo                pivot          pivot + 1                   hi
          *
          */
-    }
 
+        int pivot = partition(a,lo,hi);
+
+        m_pivot_sort(a,lo,pivot);
+        m_pivot_sort(a,pivot+1,hi);
+    }
 
     /**
      * pivot을 기준으로 파티션을 나누기 위한 약한 정렬 메소드
      *
-     * @param a		정렬 할 배열
-     * @param left	현재 배열의 가장 왼쪽 부분
-     * @param right	현재 배열의 가장 오른쪽 부분
-     * @return		최종적으로 위치한 피벗의 위치(lo)를 반환
+     * @param a     정렬 할 배열
+     * @param left  현재 배열의 가장 왼쪽 부분
+     * @param right 현재 배열의 가장 오른쪽 부분
+     * @return 최종적으로 위치한 피벗의 위치(hi)를 반환
      */
-    private static int partition(int []a ,int left,int right){
-        int lo = left;
-        int hi = right;
-        int pivot = a[right]; // 부분리스트의 오른쪽 요소를 피벗으로 설정
+    private static int partition(int[] a, int left, int right) {
+        // lo와 hi는 각각 배열의 끝에서 1 벗어난 위치부터 시작한다.
+        int lo = left - 1;
+        int hi = right - 1;
+        int pivot = a[(left + right) / 2]; //부분리스트 중간 요소를 피벗으로 설정
 
-        // lo가 hi보다 작을 때 까지만 반복한다.
-        while(lo>hi) {
-
+        while (true) {
             /*
-             * hi가 lo보다 크면서, lo의 요소가 pivot보다 큰 원소를
-             * 찾을 떄 까지 lo를 증가시킨다.
+             * 1 증가시키고 난 뒤의 lo 위치의 요소가 pivot보다 큰 요소를
+             * 찾을 떄 까지 반복한다.
              */
-            while(a[lo]<pivot && hi>lo){
+            do {
                 lo++;
-            }
+            } while (a[lo] < pivot);
+
 
             /*
-             * hi가 lo보다 크면서, hi의 요소가 pivot보다 작거나 같은 원소를
-             * 찾을 떄 까지 hi를 감소시킨다.
+             * 1 감소시키고 난 뒤의 hi 위치가 lo보다 크거나 같은 위치이면서
+             * hi위치의 요소가 pivot보다 작은 요소를 찾을 떄 까지 반복한다.
              */
-            while(hi>lo && a[hi]>= pivot){
+            do {
                 hi--;
+            } while (hi >= lo && a[hi] < pivot);
+
+            /*
+             * 만약 hi가 lo보다 크지 않다면(엇갈린다면) swap하지 않고 hi를 리턴한다.
+             */
+            if (hi <= lo) {
+                return hi;
             }
+
             // 교환 될 두 요소를 찾았으면 두 요소를 바꾼다.
-            swap(a,lo,hi);
+            swap(a, lo, hi);
         }
-        /*
-         *  마지막으로 맨 처음 pivot으로 설정했던 위치(a[right])의 원소와
-         *  hi가 가리키는 원소를 바꾼다.
-         */
-        swap(a,right,hi);
-
-        // 두 요소가 교환되었다면 피벗이었던 요소는 hi에 위치하므로 hi를 반환한다.
-        return hi;
-
     }
 
-
-    private static void swap(int[] a, int i, int j) {
-        int temp = a[i];
-        a[i] = a[j];
-        a[j] = temp;
+        private static void swap ( int[] a, int i, int j){
+            int temp = a[i];
+            a[i] = a[j];
+            a[j] = temp;
+        }
     }
-}
--- src/algorithm/quickSort/RightPivotQuickSort.java
+++ src/algorithm/quickSort/RightPivotQuickSort.java
@@ -1,62 +1,117 @@
 package algorithm.quickSort;
 
 public class RightPivotQuickSort {
-
-    public static void sort(int[] a) {
-        m_pivot_sort(a, 0, a.length - 1);
+    public static void sort(int[]a){
+        r_pivot_sort(a,0,a.length-1);
     }
 
-    private static void m_pivot_sort(int[] a, int lo, int hi) {
+    /**
+     *  오른쪽 피벗 선택 방식
+     * @param a		정렬할 배열
+     * @param lo	현재 부분배열의 왼쪽
+     * @param hi	현재 부분배열의 오른쪽
+     */
+    private static void r_pivot_sort(int[]a,int lo,int hi){
+        /*
+         *  lo가 hi보다 크거나 같다면 정렬 할 원소가
+         *  1개 이하이므로 정렬하지 않고 return한다.
+         */
 
+        if(lo>= hi){
+            return;
+        }
+
+        /*
+         * 피벗을 기준으로 요소들이 왼쪽과 오른쪽으로 약하게 정렬 된 상태로
+         * 만들어 준 뒤, 최종적으로 pivot의 위치를 얻는다.
+         *
+         * 그리고나서 해당 피벗을 기준으로 왼쪽 부분리스트와 오른쪽 부분리스트로 나누어
+         * 분할 정복을 해준다.
+         *
+         * [과정]
+         *
+         * Partitioning:
+         *
+         *         left part                right part       a[right]
+         * +---------------------------------------------------------+
+         * |    element < pivot    |    element >= pivot   |  pivot  |
+         * +---------------------------------------------------------+
+         *
+         *
+         *  result After Partitioning:
+         *
+         *         left part         a[hi]          right part
+         * +---------------------------------------------------------+
+         * |   element < pivot    |  pivot  |    element >= pivot    |
+         * +---------------------------------------------------------+
+         *
+         *
+         *  result : pivot = hi
+         *
+         *
+         *  Recursion:
+         *
+         * r_pivot_sort(a, lo, pivot - 1)     r_pivot_sort(a, pivot + 1, hi)
+         *
+         *         left part                           right part
+         * +-----------------------+             +-----------------------+
+         * |   element <= pivot    |    pivot    |    element > pivot    |
+         * +-----------------------+             +-----------------------+
+         * lo                pivot - 1        pivot + 1                 hi
+         *
+         */
     }
+
 
     /**
      * pivot을 기준으로 파티션을 나누기 위한 약한 정렬 메소드
      *
-     * @param a     정렬 할 배열
-     * @param left  현재 배열의 가장 왼쪽 부분
-     * @param right 현재 배열의 가장 오른쪽 부분
-     * @return 최종적으로 위치한 피벗의 위치(hi)를 반환
+     * @param a		정렬 할 배열
+     * @param left	현재 배열의 가장 왼쪽 부분
+     * @param right	현재 배열의 가장 오른쪽 부분
+     * @return		최종적으로 위치한 피벗의 위치(lo)를 반환
      */
-    private static int partition(int[] a, int left, int right) {
-        // lo와 hi는 각각 배열의 끝에서 1 벗어난 위치부터 시작한다.
-        int lo = left - 1;
-        int hi = right - 1;
-        int pivot = a[(left + right) / 2]; //부분리스트 중간 요소를 피벗으로 설정
+    private static int partition(int []a ,int left,int right){
+        int lo = left;
+        int hi = right;
+        int pivot = a[right]; // 부분리스트의 오른쪽 요소를 피벗으로 설정
 
-        while (true) {
+        // lo가 hi보다 작을 때 까지만 반복한다.
+        while(lo>hi) {
+
             /*
-             * 1 증가시키고 난 뒤의 lo 위치의 요소가 pivot보다 큰 요소를
-             * 찾을 떄 까지 반복한다.
+             * hi가 lo보다 크면서, lo의 요소가 pivot보다 큰 원소를
+             * 찾을 떄 까지 lo를 증가시킨다.
              */
-            do {
+            while(a[lo]<pivot && hi>lo){
                 lo++;
-            } while (a[lo] < pivot);
-
-
-            /*
-             * 1 감소시키고 난 뒤의 hi 위치가 lo보다 크거나 같은 위치이면서
-             * hi위치의 요소가 pivot보다 작은 요소를 찾을 떄 까지 반복한다.
-             */
-            do {
-                hi--;
-            } while (hi >= lo && a[hi] < pivot);
-
-            /*
-             * 만약 hi가 lo보다 크지 않다면(엇갈린다면) swap하지 않고 hi를 리턴한다.
-             */
-            if (hi <= lo) {
-                return hi;
             }
 
+            /*
+             * hi가 lo보다 크면서, hi의 요소가 pivot보다 작거나 같은 원소를
+             * 찾을 떄 까지 hi를 감소시킨다.
+             */
+            while(hi>lo && a[hi]>= pivot){
+                hi--;
+            }
             // 교환 될 두 요소를 찾았으면 두 요소를 바꾼다.
-            swap(a, lo, hi);
+            swap(a,lo,hi);
         }
+        /*
+         *  마지막으로 맨 처음 pivot으로 설정했던 위치(a[right])의 원소와
+         *  hi가 가리키는 원소를 바꾼다.
+         */
+        swap(a,right,hi);
+
+        // 두 요소가 교환되었다면 피벗이었던 요소는 hi에 위치하므로 hi를 반환한다.
+        return hi;
+
     }
 
-        private static void swap ( int[] a, int i, int j){
-            int temp = a[i];
-            a[i] = a[j];
-            a[j] = temp;
-        }
+
+    private static void swap(int[] a, int i, int j) {
+        int temp = a[i];
+        a[i] = a[j];
+        a[j] = temp;
     }
+}
