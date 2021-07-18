399fd86ff5f28f1951312700dde7537897d8152f
ryudongjae <rdj10149@gmail.com>
2021-07-18 17:27:03 +0900

    BeakJoon_1541(잃어버린 괄호)

---
+++ src/beak_joon/BeakJoon_1541.java
@@ -0,0 +1,37 @@
+package beak_joon;
+
+import javax.management.StandardEmitterMBean;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BeakJoon_1541 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int sum = Integer.MAX_VALUE;
+        StringTokenizer st = new StringTokenizer(br.readLine(),"-");
+
+
+        while(st.hasMoreTokens()){
+            int t = 0;
+
+            StringTokenizer st_p = new StringTokenizer(br.readLine(),"+");
+
+            while (st_p.hasMoreTokens()){
+                t += Integer.parseInt(st_p.nextToken());
+            }
+
+            if(sum == Integer.MAX_VALUE){
+                sum = t;
+            }else{
+                sum -= t;
+            }
+
+
+        }
+
+        System.out.println(sum);
+    }
+}
