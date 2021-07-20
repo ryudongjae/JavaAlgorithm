0f5c3a14f2674af764dd318400611b12ab936f4d
ryudongjae <rdj10149@gmail.com>
2021-07-20 14:43:24 +0900

    BeakJoon_1934(최소 공배수)

---
+++ src/beak_joon/BeakJoon_1934.java
@@ -0,0 +1,39 @@
+package beak_joon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BeakJoon_1934 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br  = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+
+        StringBuilder sb = new StringBuilder();
+
+        StringTokenizer st;
+
+        for(int i = 0; i < N; i++){
+            st = new StringTokenizer(br.readLine()," ");
+
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+
+            int d = gcd(a,b);
+
+            sb.append(a*b/d);
+        }
+        System.out.println(sb);
+    }
+
+    static int gcd(int a, int b) {
+       while(b != 0){
+           int r = a%b;
+           a = b;
+           b = r;
+       }
+        return a;
+    }
+}
