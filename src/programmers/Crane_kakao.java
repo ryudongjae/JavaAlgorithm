af7427e92f27560101a3ebe12661bc327740b927
ryudongjae <rdj10149@gmail.com>
2021-07-23 16:39:55 +0900

    프로그래머스(크레인 인형 뽑기)

---
+++ src/programmers/Crane_kakao.java
@@ -0,0 +1,33 @@
+package programmers;
+
+import java.util.Stack;
+
+/**
+ *  level 1
+ * 크레인 인형 뽑기
+ */
+public class Crane_kakao {
+    public int solution(int[][] board, int[] moves) {
+        int answer = 0;
+
+        Stack<Integer> stack = new Stack<>();
+        stack.push(0);
+
+        for (int move : moves) {
+            for (int j = 0; j < board.length; j++) {
+                if (board[j][move - 1] != 0) {
+                    if (stack.peek() == board[j][move - 1]) {
+                        stack.pop();
+                        answer += 2;
+                    } else {
+                        stack.push(board[j][move - 1]);
+                    }
+                    board[j][move - 1] = 0;
+                    break;
+                }
+            }
+        }
+
+        return answer;
+    }
+}
