d4a28968f30ff67d7190edf0e8ae78e865726dba
ryudongjae <rdj10149@gmail.com>
2021-07-25 14:36:07 +0900

    프로그래머스(행열 덧셈)

---
+++ src/programmers/LinePlus.java
@@ -0,0 +1,16 @@
+package programmers;
+
+/**
+ * 행열 덧셈
+ */
+public class LinePlus {
+    public int[][] solution(int[][] arr1, int[][] arr2) {
+        int[][] answer = new int[arr1.length][arr1[0].length];
+        for(int i = 0; i< answer.length; i++){
+            for (int j = 0; j < answer[i].length; i++){
+                answer[i][j] =arr1[i][j] + arr2[i][j];
+            }
+        }
+        return answer;
+    }
+}
