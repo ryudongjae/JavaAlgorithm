d9afd7975b72a3cd2dae2c90503d2dbe8cf31868
ryudongjae <rdj10149@gmail.com>
2021-07-27 17:02:42 +0900

    프로그래머스(LV.2) (최솟값 만들기)

---
+++ src/programmers/Minimum_value.java
@@ -0,0 +1,18 @@
+package programmers;
+
+import java.util.Arrays;
+
+public class Minimum_value {
+    public int solution(int []A, int []B) {
+        int answer = 0;
+        Arrays.sort(A);
+        Arrays.sort(B);
+
+        for(int i =0; i < A.length; i++){
+            answer += A[i] *B[A.length-i-1];
+        }
+        System.out.println(answer);
+
+        return answer;
+    }
+}
