a3401ddb4a4e48eeb718aa5a881c933166393423
ryudongjae <rdj10149@gmail.com>
2021-07-29 15:36:29 +0900

    프로그래머스 LV.3(야근 지수)

---
+++ src/programmers/Night_Work.java
@@ -0,0 +1,32 @@
+package programmers;
+
+import java.util.Collections;
+import java.util.PriorityQueue;
+
+/**
+ * 야근 지수
+ */
+public class Night_Work {
+    public long solution(int n, int[] works) {
+        long answer = 0;
+        java.util.PriorityQueue<Long> Nw = new PriorityQueue<>(Collections.reverseOrder());//Collections.reverseOrder() 내림 차순으로 정렬.
+
+        long sum = 0;
+        for (long work : works) {
+            Nw.add(work);
+            sum+=work;
+        }
+        if(sum <= n) return 0;
+
+        for(int i =0; i<n; i++){
+            long now = Nw.poll();
+            Nw.add(now-1);
+        }
+
+        for (Long s : Nw) {
+            answer +=(s*s);
+        }
+        return answer;
+
+    }
+}
