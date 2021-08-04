5e92fe7175aee6ab14a48ccd3e12a32fe2f36a97
ryudongjae <rdj10149@gmail.com>
2021-08-04 14:33:59 +0900

    프로그래머스 LV.1(홀수와 짝수)

---
+++ src/programmers/EvenOdd.java
@@ -0,0 +1,11 @@
+package programmers;
+
+public class EvenOdd {
+    public String solution(int num) {
+        if (num%2 ==  0){
+            return "Even";
+        }else{
+            return "Odd";
+        }
+    }
+}
