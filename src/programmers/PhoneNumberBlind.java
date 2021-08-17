f721448e737077bbab278f80e5173813e78acf1d
ryudongjae <rdj10149@gmail.com>
2021-08-17 17:51:08 +0900

    프로그래머스 LV.1(핸드폰 번호 가리기)

---
+++ src/programmers/PhoneNumberBlind.java
@@ -0,0 +1,13 @@
+package programmers;
+
+public class PhoneNumberBlind {
+    public String solution(String phone_number) {
+        String answer = "";
+        for(int i = 0 ; i < phone_number.length(); i++){
+            if(i < phone_number.length()-4)answer+="*";
+            else answer += phone_number.charAt(i);
+        }
+
+        return answer;
+    }
+}
