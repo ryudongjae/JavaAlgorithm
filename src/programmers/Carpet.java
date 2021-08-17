16db3214a49e244adfebf9d323aba2347c8b7a30
ryudongjae <rdj10149@gmail.com>
2021-08-17 18:48:35 +0900

    프로그래머스 LV.2(카펫)

--- src/programmers/Carpet.java
+++ src/programmers/Carpet.java
@@ -1,24 +1,28 @@
 package programmers;
 
+/**
+ * 카펫
+ */
 public class Carpet {
     public int[] solution(int brown, int red) {
+        int[] answer = {};
+        int sum = brown + red;
 
-        int height = 0;
-        int width = 0;
-        for (height = 3; height <= (int) (brown + 4) / 2; height++) {
+        for(int i = 3; i <= sum; i++){
+            if(sum %i == 0){
+                int col = sum /i; //가로
+                int row = sum / col; //세로
 
-            width = ((brown + 4) / 2) - height;
-            if (width < height) {
-                break;
-            }
 
-            int redCount = (width - 2) * (height - 2);
-            if (red == redCount) {
-                break;
+                int a = col -2;  //red의 가로
+                int b = row -2;  //red의 세로
+
+                if(a*b ==red && col >= row){
+                    return new int[]{col,row};
+                }
             }
         }
 
-        int[] answer = new int[] { width, height };
         return answer;
     }
 }
