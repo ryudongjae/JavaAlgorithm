549bd6ca1feb75b71fc5c478b611d56b734de8a0
ryudongjae <rdj10149@gmail.com>
2021-08-18 16:24:05 +0900

    프로그래머스 LV.3(숫자 게임)

---
+++ src/programmers/NumberGame.java
@@ -0,0 +1,21 @@
+package programmers;
+
+
+import java.util.Arrays;
+
+public class NumberGame {
+    public int solution(int[] A, int[] B) {
+        int answer = 0;
+        Arrays.sort(A);
+        Arrays.sort(B);
+
+
+        for(int i_a = A.length-1 , i_b = B.length-1; i_a >= 0; i_a--) {
+            if (B[i_b] > A[i_a]){
+                answer++;
+                i_b--;
+            }
+        }
+        return answer;
+    }
+}
