4622444cd2fd12092f334a18b432fc9bcd05fc69
ryudongjae <rdj10149@gmail.com>
2021-09-08 15:54:27 +0900

    프로그래머스 Lv.2(점프와 순간이동)

---
+++ src/programmers/JumpAndTeleport.java
@@ -0,0 +1,17 @@
+package programmers;
+
+public class JumpAndTeleport {
+    public int solution(int n) {
+        int ans = 0;
+        while (n != 0){
+            if (n % 2 == 0){
+                n/=2;
+            }else{
+                n--;
+                ans++;
+            }
+        }
+
+        return ans;
+    }
+}
