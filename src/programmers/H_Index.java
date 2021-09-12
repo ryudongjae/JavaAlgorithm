f2bd664908929a81ca1f417bde6552441b8711e7
ryudongjae <rdj10149@gmail.com>
2021-09-12 22:20:22 +0900

    프로그래머스 Lv.2(H-Index)

---
+++ src/programmers/H_Index.java
@@ -0,0 +1,20 @@
+package programmers;
+
+import java.util.Arrays;
+
+public class H_Index {
+    public int solution(int[] citations) {
+        int answer = 0;
+        Arrays.sort(citations);
+
+        for(int i = 0; i < citations.length; i++){
+            int h = citations.length - i;
+
+            if (citations[i] >= h){
+                answer = h;
+                break;
+            }
+        }
+        return answer;
+    }
+}
