6178f9ed3239c1bcb763f1a46f84484ae5679aa4
ryudongjae <rdj10149@gmail.com>
2021-09-19 19:27:07 +0900

    프로그래머스 Lv.1 (예산)

---
+++ src/programmers/Budget.java
@@ -0,0 +1,24 @@
+package programmers;
+
+import java.util.Arrays;
+
+public class Budget {
+    public int solution(int[] d, int budget) {
+        int answer = 0;
+        int result = 0;
+        Arrays.sort(d);
+
+        for(int i = 0; i < d.length; i++){
+            result += d[i];
+            if (result > budget){
+                answer = i;
+                break;
+            }
+        }
+
+        if(result <= budget){
+            answer = d.length;
+        }
+        return answer;
+    }
+}
