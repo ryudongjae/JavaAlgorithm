e1f04cbf871dd7f01869fee6b74a877cd517fe06
ryudongjae <rdj10149@gmail.com>
2021-09-23 17:49:41 +0900

    프로그래머스 Lv.1 (음양 더하기)

---
+++ src/programmers/NegativePositivePlus.java
@@ -0,0 +1,15 @@
+package programmers;
+
+public class NegativePositivePlus {
+    public int solution(int[] absolutes, boolean[] signs) {
+        int answer = 0;
+        for(int i = 0 ; i < signs.length; i++){
+            if(signs[i]){
+                answer += absolutes[i];
+            }else{
+                answer -= absolutes[i];
+            }
+        }
+        return answer;
+    }
+}
