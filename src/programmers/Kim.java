44082e6c4e7706f66d2301d58d52b3a1b92d437b
ryudongjae <rdj10149@gmail.com>
2021-09-23 17:46:18 +0900

    프로그래머스 Lv.1 (서울에서 김서방 찾기)

---
+++ src/programmers/Kim.java
@@ -0,0 +1,13 @@
+package programmers;
+
+public class Kim {
+    public String solution(String[] seoul) {
+        String answer = "";
+        for(int i = 0; i < seoul.length; i++){
+            if(seoul[i].equals("Kim")){
+                 return answer = "김서방은 " + i+"에 있다";
+            }
+        }
+        return answer;
+    }
+}
