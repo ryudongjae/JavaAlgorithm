c8a9785f74660f6fe549e38520f20df1455572a9
ryudongjae <rdj10149@gmail.com>
2021-09-24 14:01:26 +0900

    프로그래머스 Lv.1 (약수의 합)

---
+++ src/programmers/AddFactor.java
@@ -0,0 +1,11 @@
+package programmers;
+
+public class AddFactor {
+    public int solution(int n) {
+        int answer = 0;
+        for(int i = 1; i <= n/2; i++){
+            if(n%i == 0)answer+= i;
+        }
+        return answer + n;
+    }
+}
