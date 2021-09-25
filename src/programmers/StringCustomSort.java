613cb835f8b36851fb49b71ceef8f780f31c6015
ryudongjae <rdj10149@gmail.com>
2021-09-25 20:08:45 +0900

    프로그래머스 Lv.1 (문자열 내 마음대로 정렬하기)

---
+++ src/programmers/StringCustomSort.java
@@ -0,0 +1,24 @@
+package programmers;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class StringCustomSort {
+    public String[] solution(String[] strings, int n) {
+        String[] answer = {};
+        ArrayList<String>list = new ArrayList<>();
+
+        for(int i = 0; i < strings.length; i++){
+            list.add(strings[i].charAt(n) + strings[i]);
+        }
+
+        Collections.sort(list);
+
+        answer = new String[list.size()];
+
+        for (int i = 0; i< list.size(); i++){
+            answer[i] = list.get(i).substring(1,list.get(i).length());
+        }
+        return answer;
+    }
+}
