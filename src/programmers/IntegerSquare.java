8a10f98fecd0e1ac8269c960a1fc053b19ed9f3a
ryudongjae <rdj10149@gmail.com>
2021-09-26 15:13:02 +0900

    프로그래머스 Lv.1 (정수 제곱근 판별)

---
+++ src/programmers/IntegerSquare.java
@@ -0,0 +1,13 @@
+package programmers;
+
+public class IntegerSquare {
+    public long solution(long n) {
+        Double sqrt = Math.sqrt(n);
+
+        if(sqrt == sqrt.intValue()){
+            return (long) Math.pow(sqrt+1,2);
+        }else {
+            return -1;
+        }
+    }
+}
