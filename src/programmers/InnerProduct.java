71500ab9de481d0e04745c06183e3d085ce7c8d4
ryudongjae <rdj10149@gmail.com>
2021-09-28 13:13:57 +0900

    프로그래머스 Lv.1 (내적)

---
+++ src/programmers/InnerProduct.java
@@ -0,0 +1,11 @@
+package programmers;
+
+public class InnerProduct {
+    public int solution(int[] a, int[] b) {
+        int answer = 0;
+        for(int i = 0; i < a.length; i++){
+            answer += a[i]*b[i];
+        }
+        return answer;
+    }
+}
