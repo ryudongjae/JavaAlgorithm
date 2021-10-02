d82625bf5a0d5190928a7e102940abff29373812
ryudongjae <rdj10149@gmail.com>
2021-10-02 14:42:00 +0900

    프로그래머스 Lv.1 (약수의 개수와 덧셈)

---
+++ src/programmers/MeasureCountAdd.java
@@ -0,0 +1,17 @@
+package programmers;
+
+public class MeasureCountAdd {
+    public int solution(int left, int right) {
+        int answer = 0;
+        for (int i = left; i <= right; i++){
+            int count = 0;
+            for (int j = 1; j <= i; j++){
+                if (i%j == 0)count++;
+            }
+            if (count % 2 ==0 )answer+=i;
+            else answer -= i;
+
+        }
+        return answer;
+    }
+}
