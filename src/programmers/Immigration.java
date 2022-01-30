a9d56956f6cf28ef23c51a909832f0d2d63906a8
ryudongjae <rdj10149@gmail.com>
2022-01-30 16:09:17 +0900

    제너릭

--- src/programmers/Immigration.java
+++ src/programmers/Immigration.java
@@ -7,16 +7,17 @@ public class Immigration {
         long answer = 0;
         Arrays.sort(times);
         long left = 0;
-        long right = (long) n *times[times.length-1];
+        long right = (long) n *times[times.length-1];//가장 최악의 경우의(오래걸리는) 시간
         while(left <= right){
             long mid = (left+right)/2;
-            long sum = 0;
-            for(int i =0; i < times.length; i++){
+            long sum = 0;// 총 심사한 인원
+            for(int i =0; i < times.length; i++){//빨리 심사하는 심사관 순으로 심사처리
+
                 sum +=mid/times[i];
             }
-            if(sum < n){
+            if(sum < n){ // 해야할 인원보다 심사처리 못함 -> 시간 더 필요
                 left = mid+1;
-            }else{
+            }else{  // 해야할 인원보다 심사처리 많이함 -> 시간을 줄여서 더 최고 경우의 시간을 만든다.
                 right = mid - 1;
                 answer = mid;
             }
--- src/programmers/MathSum.java
+++ src/programmers/MathSum.java
@@ -2,21 +2,21 @@ package programmers;
 
 public class MathSum {
     public int solution(int n) {
-       int answer = 0;
+        int answer = 0;
 
-       for(int i = 0; i <= n; i++){
-           int sum = 0;
-           for(int j = i; j<=n; j++){
-               sum+= j;
+        for(int i=1; i<=n; i++) {
+            int sum = 0;
+            for(int j=i; j<=n; j++) {
+                sum += j;
 
-               if(sum == n){
-                   answer++;
-                   break;
-               }else if(sum > n){
-                   break;
-               }
-           }
-       }
-       return  answer;
+                if(sum==n) {
+                    answer++;
+                    break;
+                } else if(sum>n) {
+                    break;
+                }
+            }
+        }
+        return answer;
     }
 }
